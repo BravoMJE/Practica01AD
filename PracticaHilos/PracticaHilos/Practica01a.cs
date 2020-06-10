598518c7c3ddce2fcda750bbbec3a0ae5562e72c
JOSEPH ESTUARDO BRAVO MACAS <joseph.bravo@epn.edu.ec>
2020-06-10 19:18:22 -0400

    Finalizacion de parte 1 creacion de hilos

--- PracticaHilos/PracticaHilos/Practica01a.cs
+++ PracticaHilos/PracticaHilos/Practica01a.cs
@@ -1,8 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ************************************************************************
+// Practica 01
+// Joseph Bravo, Esteban Machado
+// Fecha de realizacion: 04/06/2020
+// Fecha de entrega: 08/06/2020
+// Resultados:
+//* El codigo permita visualizar como se asigna el quantum
+// a cada hilo
+// Conclusiones:
+//*
+// Recomendaciones:
+//*
+// ************************************************************************
+
+using System;
+using System.Threading;
 
 namespace PracticaHilos
 {
@@ -10,6 +21,26 @@ namespace PracticaHilos
     {
         static void Main(string[] args)
         {
+            //Creacion de hilo este ejecutara el metodo ImprimirNumeros
+            Thread hiloTrabajador = new Thread(delegate () { ImprimirNumeros(); });
+            hiloTrabajador.Start();
+
+            //Llamada al metodo ImprimirNumeros que nos permitira ver
+            //la asignacion de tiempos del procesador
+            ImprimirNumeros();
+
+            Console.ReadLine();
         }
+
+
+        //Metodo que imprime numeros del 1 a 50
+        static void ImprimirNumeros()
+        {
+            Console.WriteLine("Empezando ….");
+            for (int i = 1; i < 50; i++)
+                Console.WriteLine(i);
+        }
+
+        
     }
 }
